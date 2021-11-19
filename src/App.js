901922ec114e422e2e405017c3a833bdda6866a2
MyeongHoonNam <codenmh0822@naver.com>
2021-11-19 15:51:47 +0900

    feat: Header 컴포넌트로 모듈화 구현

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import { Global } from '@emotion/react'
 import { GlobalStyles } from './styles/GlobalStyles'
-
-const Header = styled.div`
-  padding-top: 10%;
-  padding-bottom: 10%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-`
-
-const Title = styled.div`
-  font-size: 148px;
-  font-family: 'Pathway Gothic One', sans-serif;
-`
-
-const Description = styled.div`
-  margin-top: 30px;
-  font-size: 22px;
-
-  span {
-    color: green;
-  }
-`
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyles} />
-      <Header>
-        <Title>Formatter</Title>
-        <Description>
-          Format your &lt;<span>input</span>/&gt; content when you are typing
-        </Description>
-      </Header>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  padding-top: 10%;
+  padding-bottom: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`
+
+const Title = styled.div`
+  font-size: 148px;
+  font-family: 'Pathway Gothic One', sans-serif;
+`
+
+const Description = styled.div`
+  margin-top: 30px;
+  font-size: 22px;
+
+  span {
+    color: green;
+  }
+`
+
+const Header = () => {
+  return (
+    <Container>
+      <Title>Formatter</Title>
+      <Description>
+        Format your &lt;<span>input</span>/&gt; content when you are typing
+      </Description>
+    </Container>
+  )
+}
+
+export default Header
