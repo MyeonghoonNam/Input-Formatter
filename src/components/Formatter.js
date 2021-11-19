3b485176a9837fe5d3ce32422fae3e1057e75815
MyeongHoonNam <codenmh0822@naver.com>
2021-11-19 20:22:14 +0900

    feat: Formatter 컴포넌트 구현

---
+++ src/components/Formatter.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.article`
+  padding: 60px 0;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+
+const Title = styled.div`
+  padding-bottom: 30px;
+  font-size: 36px;
+  font-weight: bold;
+  font-family: 'Roboto', sans-serif;
+  color: #272822;
+  text-align: center;
+`
+
+const Input = styled.input`
+  border: 3px solid #e6e6e6;
+  border-radius: 5px;
+  background-color: #fff;
+  line-height: 60px;
+  vertical-align: middle;
+  height: 66px;
+  font-size: 32px;
+  padding: 0 15px;
+  font-family: 'Pathway Gothic One', sans-serif;
+`
+
+const Formatter = ({ formatter }) => {
+  const { type, title, message } = formatter
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Input placeholder={message} />
+    </Container>
+  )
+}
+
+export default Formatter
